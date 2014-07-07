bff8a0d23060592c0ee8b9f11869404440c60b09
Larrytc <larry_0904@hotmail.com>
2014-07-06 23:31:32 -0500

    app inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Holamundo::Application.config.secret_key_base = '491c1c52be936fc212988a646f28bf26d9215b03c87cf2084b9abb25ac850840d9f701e462ca899338d2eca3fc95dd4f259a6a7b5801017272a363f54399aaf3'
